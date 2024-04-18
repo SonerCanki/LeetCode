a0f792027e3057415bf7facec3d377b4202d4d94
Soner Canki <sonercanki01@gmail.com>
2024-04-18 13:20:11 +0300

    Palindrome Number

--- 3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters/Program.cs
+++ 3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters/Program.cs
@@ -1,21 +1,25 @@
 ﻿
-var s = "abcabcbb";
+var s = "aab";
 
-var lenght = LengthOfLongestSubstring(s);
-Console.WriteLine(lenght);
+var length = LengthOfLongestSubstring(s);
+Console.WriteLine(length);
+
 int LengthOfLongestSubstring(string s)
 {
-    var dic = new Dictionary<char, int>();
-    var lenght = 0;
+    Dictionary<char, int> dic = new Dictionary<char, int>();
+    var length = 0;
     for (int i = 0; i < s.Length; i++)
     {
         dic.Add(s[i], i);
-        if (dic.ContainsKey(s[i]))
-        {
-            lenght = lenght > dic.Keys.Count ? lenght : dic.Keys.Count;
-            dic.Clear();
-        }
-        
     }
-    return lenght;
+
+    return length;
 }
+
+
+
+//if (dic.ContainsKey(s[i]))
+//        {
+//            length = length > dic.Keys.Count ? length : dic.Keys.Count;
+//            dic.Clear();
+//        }
---
+++ 9. Palindrome Number/9. Palindrome Number/Program.cs
@@ -0,0 +1,33 @@
+﻿
+IsPalindromeNumber(1215);
+
+bool IsPalindromeNumber(int x)
+{
+    var y = x;
+    var z = x;
+    var palindromeNumber = 0;
+    var counter = -1;
+    if (x < 0)
+    {
+        return false;
+    }
+
+    while (z > 0)
+    {
+        z = z / 10;
+        counter++;
+    }
+
+    while (y > 0)
+    {
+        int b = y % 10;
+        y = y / 10;
+        palindromeNumber += b * Convert.ToInt32(Math.Pow(10, counter));
+        counter--;
+    }
+
+    if (x == palindromeNumber)
+    { return true; }
+
+    return false;
+}
